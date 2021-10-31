e23ea180a6478e99c32d8ec3ffd71dde7df7d213
copadoje <copadoemmanuel@gmail.com>
2021-10-31 13:43:42 -0600

    base de datos

--- vacatec.sql
+++ vacatec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-10-2021 a las 04:39:36
+-- Tiempo de generación: 31-10-2021 a las 20:38:00
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.2.32
 
@@ -20,6 +20,56 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `vacatec`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `corrales`
+--
+
+CREATE TABLE `corrales` (
+  `id` int(11) NOT NULL,
+  `prom_edad` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `num_vacas` int(11) DEFAULT NULL,
+  `num_machos` int(11) DEFAULT NULL,
+  `num_hembras` int(11) DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `corrales`
+--
+
+INSERT INTO `corrales` (`id`, `prom_edad`, `status`, `fecha_inicio`, `num_vacas`, `num_machos`, `num_hembras`, `fecha_fin`) VALUES
+(1, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(2, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(3, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(4, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(5, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(6, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(7, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(8, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(9, NULL, 1, NULL, NULL, NULL, NULL, NULL),
+(10, NULL, 1, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `corrales_exis`
+--
+
+CREATE TABLE `corrales_exis` (
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `corrales_exis`
+--
+
+INSERT INTO `corrales_exis` (`cantidad`) VALUES
+(10);
 
 -- --------------------------------------------------------
 
@@ -51,6 +101,12 @@ INSERT INTO `usuarios` (`id`, `username`, `nombre`, `apellido`, `correo`, `passw
 --
 
 --
+-- Indices de la tabla `corrales`
+--
+ALTER TABLE `corrales`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -59,6 +115,12 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `corrales`
+--
+ALTER TABLE `corrales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
